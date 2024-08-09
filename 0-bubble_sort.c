fc86e15d5b0858af0c81110636f086fc5a8c8c2b
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-08-09 01:29:15 +0000

    Sorting algorithms & Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,13 +7,23 @@
  * @array: pointer to the array
  * @size: size of the array
  *
- */ 
+ */
 void bubble_sort(int *array, size_t size)
 {
-	int i = 0, j = 0;
-	int temp;
+	size_t i = 0, j = 0;
+	size_t temp;
 
-	for (i = 0; i < size; i++)
-
-
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < (size - 1 - i); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
