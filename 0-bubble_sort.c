a33a6815bb1cf8726f3db46666b85dfa074efcaf
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-08-09 01:54:13 +0000

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,9 +12,11 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i = 0, j = 0;
 	size_t temp;
+	int flag;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		flag = 0;
 		for (j = 0; j < (size - 1 - i); j++)
 		{
 			if (array[j] > array[j + 1])
@@ -23,7 +25,10 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
+				flag = 1;
 			}
 		}
+		if (flag == 0)
+			break;
 	}
 }
